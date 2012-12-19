9dbfc4e995ba89b103f39275b9f1c1fbee3f753d
Tõnis Tiigi <tonistiigi@gmail.com>
2012-12-19 13:41:07 +0200

    Fix parallel processing issues with merge

--- bench/merge.js
+++ bench/merge.js
@@ -1,31 +1,40 @@
-#!env node
 var fs = require('fs')
+var JSONStream = require('JSONStream')
 var argv = require('optimist')
-  .boolean('pfx', {describe: 'Prefix current name'})
   .usage('$0 file1 [file2] [file3]')
   .demand(1)
   .argv
 
-var files = argv._.map(function(file) {
-  return JSON.parse(fs.readFileSync(file).toString())
-})
+var files = []
+function load() {
+  if (!argv._.length) return run()
+  var file = argv._.shift()
+  var parse = fs.createReadStream(file).pipe(JSONStream.parse())
+  parse.on('data', files.push.bind(files))
+  parse.on('end', load)
+}
 
-var json = {}
+load()
 
-files.forEach(function(input) {
-  for (var key in input) {
-    if (json.key) return
-    json[key] = files.reduce(function(memo, file) {
-      if (file[key]) {
-        for (var i in file[key]) {
-          memo[i] = file[key][i]
+function run() {
+  var json = {}
+
+  files.forEach(function(input) {
+    for (var key in input) {
+      if (json.key) return
+      json[key] = files.reduce(function(memo, file) {
+        if (file[key]) {
+          for (var i in file[key]) {
+            memo[i] = file[key][i]
+          }
         }
-      }
-      return memo
-    }, {})
-  }
-})
+        return memo
+      }, {})
+    }
+  })
 
-process.stdout.write(JSON.stringify(json))
-process.stdout.write('\n')
+  process.stdout.write(JSON.stringify(json))
+  process.stdout.write('\n')
+
+}
 
